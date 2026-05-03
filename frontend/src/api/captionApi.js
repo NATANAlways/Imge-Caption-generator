6b9c0f4cf4228f1f19341daac4fd9b7831c74c87
NATANAlways <nathiskaralways@gmail.com>
2026-05-03 05:33:41 +0530

    Fix backend URL to HF Space

--- frontend/src/api/captionApi.js
+++ frontend/src/api/captionApi.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://localhost:8000/api'
+// const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000/api'
+const BASE_URL = 'https://nathis-image-caption-backend.hf.space/api'
+
 
 export async function generateCaption(imageFile, modelName) {
   const formData = new FormData()
